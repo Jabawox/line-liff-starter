543c57b84d391bb4cc40dcfe38a986073d5238e7
Jabawox <50706618+Jabawox@users.noreply.github.com>
2019-08-05 00:57:27 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -4,109 +4,16 @@ window.onload = function (e) {
     });
 };
 
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
-    document.getElementById('useridfield99').textContent = data.context.userId;
-
-
-    // openWindow call
-    document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.openWindow({
-            url: 'https://line.me'
-        });
-    });
-
-    // Script call
-    document.getElementById('Scriptbutton').addEventListener('click', function () {
-        const UserID = document.getElementById('useridfield').textContent;
-        liff.openWindow({
-            url: 'https://script.google.com/macros/s/AKfycbw-Zz9rNyK3TTxOieNmvj1Rt8BRz7h4Op4gkrJo_pXfNYUid8l7/exec?userid='+UserID
-        });
-    });
-
-    // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
-
-    // sendMessages call
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-    });
-
-    // get access token
-    document.getElementById('getaccesstoken').addEventListener('click', function () {
-        const accessToken = liff.getAccessToken();
-        document.getElementById('accesstokenfield').textContent = accessToken;
-        toggleAccessToken();
-    });
-
-    // get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
-
-            const profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            const img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
-}
-
-function toggleAccessToken() {
-    toggleElement('accesstokendata');
-}
-
-function toggleProfileData() {
-    toggleElement('profileinfo');
-}
-
-function toggleElement(elementId) {
-    const elem = document.getElementById(elementId);
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
-
     //sendmsg
     function sendmsg(){
       var FN = document.getElementById("Fname").value;
       var EM = document.getElementById("Uemail").value;
-      
+
       liff.getProfile().then(function (profile) {
         liff.sendMessages([{
             type: 'text',
-            text: "Name : " + FN + '\n' + 
-                  "E-mail : " + EM + '\n' + 
+            text: "Name : " + FN + '\n' +
+                  "E-mail : " + EM + '\n' +
                   "Uid : " + profile.userId
             //text: FN + " , " + EN + " , " + UserX
         }]).then(function () {
