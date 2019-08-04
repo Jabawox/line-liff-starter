0dc0afb66c95fa36061a4b77110acfd857422ac5
Jabawox <50706618+Jabawox@users.noreply.github.com>
2019-08-05 01:03:45 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -8,11 +8,15 @@ window.onload = function (e) {
     function sendmsg(){
       var FN = document.getElementById("Fname").value;
       var EM = document.getElementById("Uemail").value;
+      var PeaId = document.getElementById("PEAid").value;
+      var PhoneNum = document.getElementById("Phone").value;
 
       liff.getProfile().then(function (profile) {
         liff.sendMessages([{
             type: 'text',
             text: "Name : " + FN + '\n' +
+                  "PEA ID : " + PeaId + '\n' +
+                  "Phone : " + PhoneNum + '\n' +
                   "E-mail : " + EM + '\n' +
                   "Uid : " + profile.userId
             //text: FN + " , " + EN + " , " + UserX
